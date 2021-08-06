48e47d0bc50f366d733a6c58717851991ea996d7
HystERICKal <nyoroerick12@gmail.com>
2021-08-06 05:07:30 +0200

    Building the tweetBox

---
+++ twitter_clone/src/TweetBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./TweetBox.css"
+// import {Avatar, Button} from "@material-ui/core";
+
+function TweetBox() {
+    return (
+        <div className="tweetBox">
+            <form>
+                <div className="tweetBox__input">
+
+                </div>
+                {/* <Button>Tweet</Button> */}
+            </form>
+        </div>
+    )
+}
+
+export default TweetBox
